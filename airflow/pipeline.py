b3bb79738f4c74dbf7eed70130494315050fd2b3
Matheus-dCastro <matheus.vsf.castro.25@gmail.com>
2026-09-17 13:38:15 -0300

    feat: add TMDB API extraction and transformation pipeline
    - Created a new markdown file for mentoring records and project development history.
    - Implemented a new Airflow pipeline script for extracting movie data from TMDB API.
    - Added functions for API requests, data extraction, transformation, and loading into CSV.
    - Included feature engineering techniques such as age calculation, genre mapping, and data normalization.
    - Established a directory structure for storing processed data.

--- airflow/pipeline.py
+++ airflow/pipeline.py
@@ -14,12 +14,12 @@ import os
 #     start_date=datetime(2026, 9,12),
 #     schedule= "@daily" ) as dag:
 
-URL = "https://api.themoviedb.org/3/movie/top_rated"
-URL_generos= "https://api.themoviedb.org/3/genre/movie/list"
-API_KEY = "66f326ea45d4f51ff3ed4071d5f20ad0"
+URL = "https://api.themoviedb.org/3/movie/top_rated" #  mudar para variaveis do airflow
+URL_generos= "https://api.themoviedb.org/3/genre/movie/list" #  mudar para variaveis do airflow
+API_KEY = "66f326ea45d4f51ff3ed4071d5f20ad0" #  mudar para variaveis do airflow
 
 
-def api_request(URL="", api_key="", n_pages=None):
+def api_request(URL="", api_key="", n_pages=None): # requisição
     parametros = {
         "api_key": api_key,
         "language": "pt-BR",
@@ -35,7 +35,7 @@ def api_request(URL="", api_key="", n_pages=None):
         print(f"ERROR: {result.status_code}" )
 
 
-def extract_task_moves(URL,pages=0 ):
+def extract_task_moves(URL,API_KEY, pages=0 ): # extração
     all_moves = []
     
     for page in range(1, pages+1):
@@ -44,42 +44,49 @@ def extract_task_moves(URL,pages=0 ):
             all_moves.extend(requisicao["results"])
     return all_moves
 
-def extract_task_gener():
-    id_generos= api_request(URL_generos, API_KEY) # para o modelo saber oq é cada id_gener de cada filme
+def extract_task_gener(API_KEY, URL): # extraçao 
+    id_generos= api_request(URL, API_KEY) # para o modelo saber oq é cada id_gener de cada filme
     genres = []
     if(id_generos and "genres" in id_generos):
         genres.extend(id_generos["genres"])
     return genres
 
-def transform_task(moves, geners,name_df=""):
+
+
+
+def transform_task(moves, geners):
     df_all_moves =  pd.DataFrame(moves)
     
     os.makedirs("data", exist_ok=True)
     df_all_moves = df_all_moves.sort_values(by="vote_average", ascending=False) # ordena pela quanntidade de aprovação do filme pelo piblico
 
-    df_all_moves["age"] =  datetime.now().year - pd.to_datetime(df_all_moves["release_date"]).dt.year # pega a idade do filme e a quantos anos ele foi lançado
+    df_all_moves["age"] =  datetime.now().year - pd.to_datetime(df_all_moves["release_date"]).dt.year # pega a idade do filme e a quantos anos ele foi lançado feature
 
-    mapa_generos = {g["id"]: g["name"] for g in geners}
+    mapa_generos = {g["id"]: g["name"] for g in geners} # feature
 
-    for id_g, nome_g in mapa_generos.items():
+    for id_g, nome_g in mapa_generos.items(): # feature
         coluna = f"genero_{nome_g.lower().replace(' ', '_')}"
         df_all_moves[coluna] = df_all_moves["genre_ids"].apply(lambda ids: 1 if id_g in ids else 0)
         
-    df_all_moves["num_genres"] = df_all_moves["genre_ids"].apply(len)
+    df_all_moves["num_genres"] = df_all_moves["genre_ids"].apply(len) # feature
     
     df_all_moves =  df_all_moves.drop(["release_date","genre_ids","backdrop_path","poster_path","softcore","video","adult","original_title"],axis=1) # remove featores desnecessarias para analise
 
     scaler = StandardScaler()
     colunas_para_normalizar = ["vote_count","popularity"]
     df_all_moves[colunas_para_normalizar] = scaler.fit_transform(df_all_moves[colunas_para_normalizar])
+    # fazer a .fillna("") para remover 
     
-    
-    df_all_moves.to_csv(f"data/{name_df}.csv", index=False)
     return df_all_moves
 
+def load_df(df:pd.DataFrame, name_df="features"):
+    df.to_csv(f"data/{name_df}.csv", index=False)
+    return df
 
-filmes = extract_task_moves(URL, 5)
-generos = extract_task_gener()
-nome = "df_moves"
 
-transform_task(filmes, generos, nome)
+
+filmes = extract_task_moves(URL, API_KEY,5) # pega os 100 filmes
+generos = extract_task_gener(API_KEY,URL_generos) # pega os generos acossiados a cada id
+
+df=transform_task(filmes, generos)
+load_df(df)
